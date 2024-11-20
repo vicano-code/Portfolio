2f5cf59d1ad5cbe617afadcba3a54efa5a1d4570
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-11-20 16:55:55 +0100

    update resume link

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,11 +9,11 @@ const Intro = () => {
             <div id="intro"> 
                 <img src={Vic} alt="my pic" />  
                 <span id="intro-name">Victor Anokwuru</span>
-                <span id="intro-title">Software Developer</span>
+                <span id="intro-title">Software Engineer</span>
                 <span id="intro-summary">I am a highly motivated, frontend developer with an objective to utilize my skills in web design and development to create engaging and user-friendly interfaces for websites and applications, while keeping up with the latest trends and technologies in the field. I aim to work collaboratively with cross-functional teams to deliver high-quality products that meet client and user needs.</span>
                 <div id="resume-link">
-                    <a href={Resume} download>
-                        <button className="button i-button" >DOWNLOAD RESUME</button>
+                    <a href="https://drive.google.com/file/d/1pTzBoKfyoC9LEYFF-GOatGl5E06RK7NI/view?usp=sharing">
+                        <button className="button i-button" >VIEW RESUME</button>
                     </a>
                 </div>
             </div>
